b69894e31ab304ae1701e2b6b548f135fc9ff798
manie.coetzee <katGriep0921>
2017-11-11 08:21:00 +0200

    refactor

--- src/app/modules/am-logger/am-logger.module.ts
+++ src/app/modules/am-logger/am-logger.module.ts
@@ -8,3 +8,11 @@ import { CommonModule } from '@angular/common';
   declarations: []
 })
 export class AmLoggerModule { }
+
+/*
+export {Logger } from './src/am-logger/log-logger';
+export {LogConfig } from './src/am-logger/log-config';
+export {LogLevel } from './src/am-logger/log-level.enum';
+export {LogService } from './src/am-logger/log.service';
+export {LogModule} from './src/module';
+*/
