3b110b6b4121b3f0deeaea10a01ca7ed8c0c6846
manie.coetzee <katGriep0921>
2017-11-11 08:24:25 +0200

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Logger} from "./modules/am-logger/log-logger";
+import {LogService} from "./modules/am-logger/log.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  logger: Logger;
+
+  constructor(private logService: LogService){
+    this.logger = logService.getLogger(this.constructor.name);
+  }
+
+  ngOnInit(): void {
+    this.logger.info("Hello World");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import {AmLoggerModule} from "./modules/am-logger/am-logger.module";
 
 
 @NgModule({
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmLoggerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
