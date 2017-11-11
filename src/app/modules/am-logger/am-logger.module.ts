864ed836fd3ecca77131b3628f2a81c154d64f23
manie.coetzee <katGriep0921>
2017-11-11 08:37:55 +0200

    refactor

--- src/app/modules/am-logger/am-logger.module.ts
+++ src/app/modules/am-logger/am-logger.module.ts
@@ -10,11 +10,3 @@ import {LogService} from "./log.service";
   declarations: []
 })
 export class AmLoggerModule { }
-
-/*
-export {Logger } from './src/am-logger/log-logger';
-export {LogConfig } from './src/am-logger/log-config';
-export {LogLevel } from './src/am-logger/log-level.enum';
-export {LogService } from './src/am-logger/log.service';
-export {LogModule} from './src/module';
-*/
