8043af7c6b51bfb681f44a59d0d06e3ec76db8c1
manie.coetzee <katGriep0921>
2018-03-31 10:47:01 +0200

    fix for ng-packagr complaining about an error

--- public_api.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/app/modules/am-logger/am-logger.module'
-export * from './src/app/modules/am-logger/log-logger';
-export * from './src/app/modules/am-logger/log-config';
-export * from './src/app/modules/am-logger/log-level.enum';
-export * from './src/app/modules/am-logger/log.service';
---
+++ src/public_api.ts
@@ -0,0 +1,5 @@
+export * from './app/modules/am-logger/am-logger.module'
+export * from './app/modules/am-logger/log-logger';
+export * from './app/modules/am-logger/log-config';
+export * from './app/modules/am-logger/log-level.enum';
+export * from './app/modules/am-logger/log.service';
